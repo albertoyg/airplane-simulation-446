3e1640a07cce44da47410b6ec65187ffde1c823a
Eric-Showers <ejs1357@hotmail.com>
2023-04-09 13:29:22 -0700

    Add TODOs for next steps

--- prototype.py
+++ prototype.py
@@ -2,6 +2,8 @@ import random
 import matplotlib.pyplot as plt
 
 
+# Prints a visual representation of the plane
+#TODO: Draw the plane with colors and shapes instead of ASCII
 def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets):
     str_len = 7
 
@@ -44,21 +46,51 @@ def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets):
     print("_" * (num_rows * (str_len + 1) + 19))
 
 
-def run_simulation(num_rows, num_cols, draw=False):
-    # TODO: Take ordering in as a simulation parameter
-    num_passengers = num_rows * num_cols * 2
+def run_simulation(num_rows, num_cols, queue, draw=False):
+    #TODO: Track simulation stats such as the following
+    # Time each passenger spends standing on the plane
+    # Time each passenger spends accessing their seat
+    # Time each passenger spends storing baggage
+    # Average number of passengers standing on the plane at any one time
+    # Total time to seat all passengers
 
+    num_passengers = len(queue)
+    max_seat_num = num_rows * num_cols * 2
+    assert  max_seat_num >= max(queue), \
+        f"Passenger has number {max(queue)} but only {max_seat_num} seats are available"
+
+    # Tracks which aisle positions are ocupied by which passengers
     aisle_rows = [-1 for i in range(num_rows)]
-    occupied_seats = [[-1, -1] * num_cols for i in range(num_rows)]
-    passengers = [i for i in range(num_passengers)]
-    tickets = [{'row': i, 'seat': j} for i in range(num_rows) for j in range(num_cols * 2)]
-    random.shuffle(passengers)
-    queue = passengers.copy()
-    # Passenger i is assigned seat tickets[i]
-
+    """
+    TODO: 
+        -Currently hard-coded to take 3 time steps to store baggage
+        -Should be change to exponential distribution
+        -Only some passengers should store baggage, this could be a parameter of the simulation 
+            eg. % of passengers storing baggage"""
+    # Timers for which aisle positions are blocked by passengers storing baggage
     storing_baggage = [0 for i in range(num_rows)]
-    num_seated = 0
+    """
+    TODO:
+        -Should take additional time to get in their seat, idependent of whether they have stored baggage or not
+        -Should take longer depending on the number of seated passengers between the aisle and their seat
+            eg. passenger has middle seat, but someone is already seated in the aisle seat
+        -My intuition is this timer should be set according to an exponential distribution 
+            with a mean of 1+2*(number of passengers seated between aisle and desired seat) time steps
 
+        accessing_seat array is currently unused
+    """
+    # Timers for which aisle positions are blocked by passengers accessing their seat
+    accessing_seat = [0 for i in range(num_rows)]
+    # Tracks which seats are occupied by which passengers
+    occupied_seats = [[-1, -1] * num_cols for i in range(num_rows)]
+    # Passenger i is assigned seat tickets[i]
+    tickets = [
+        {'row': i, 'seat': j}
+        for i in range(num_rows)
+        for j in range(num_cols * 2)
+    ]
+
+    num_seated = 0
     time_steps = 0
     # Loop until all passengers are seated
     while num_seated < num_passengers:
@@ -71,38 +103,64 @@ def run_simulation(num_rows, num_cols, draw=False):
             ticket = tickets[passenger]
             # Check if passenger is in the row of their assigned seat
             if ticket['row'] == aisle_row:
-                # Start storing baggage timer
+                # TODO: Check if passenger has to store baggage
+                # TODO: Set timer using exponential distribution
+                # If storing baggage timer is zero, set it
                 if storing_baggage[aisle_row] == 0:
                     storing_baggage[aisle_row] = 3
                 else:
+                    # Decrement timer
                     storing_baggage[aisle_row] -= 1
                     # If timer is up then seat the passenger
                     if storing_baggage[aisle_row] == 0:
+                        # TODO: Start accessing seat timer once done storing baggage
                         occupied_seats[ticket['row']][ticket['seat']] = passenger
                         aisle_rows[aisle_row] = -1
                         num_seated += 1
+            # Passenger is not in the row of their assigned seat
             else:
                 # Check if next aisle row is empty
                 if aisle_rows[aisle_row + 1] == -1:
+                    # Move passenger up the aisle
                     aisle_rows[aisle_row + 1] = passenger
                     aisle_rows[aisle_row] = -1
 
+        # If aisle has space, the next passenger in queue enters the plane
         if len(queue) > 0 and aisle_rows[0] == -1:
             passenger = queue.pop(0)
             aisle_rows[0] = passenger
         if draw == True:
             draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets)
         time_steps += 1
-    return time_steps, passengers
+    return time_steps
 
+
+plane_rows = 20
+plane_cols = 3
 
 times = []
 best_time = 0
 best_ordering = []
 worst_time = 0
 worst_ordering = []
-for i in range(10000):
-    time, ordering = run_simulation(15, 5, draw=False)
+
+"""
+TODO: Instead of orderings being completely random, they should be generated using 
+rules by which passengers could realistically be ordered. For example:
+
+-Passengers are ordered by "zone", where the rows are broken up into zones and we 
+    select which zones go first. Row & column orderings within zones would still be random.
+-Passengers are ordered by seat column eg. windows seats first, then middle, then aisle. 
+    Row orderings would still be random.
+
+Ultimately we will be comparing the metrics of different orderings, so we want several methods
+of generating orderings to test.
+"""
+
+for i in range(1000):
+    ordering = [i for i in range(plane_rows * plane_cols * 2)]
+    random.shuffle(ordering)
+    time = run_simulation(plane_rows, plane_cols, ordering.copy(), draw=False)
     times.append(time)
     if time > worst_time:
         worst_time = time
