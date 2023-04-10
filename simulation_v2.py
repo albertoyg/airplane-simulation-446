11bd8b33a3c120e1a14904d967e74b53907a59dd
Eric-Showers <ejshowers@gmail.com>
2023-04-10 15:49:37 -0700

    Debug v2

--- simulation_v2.py
+++ simulation_v2.py
@@ -1,7 +1,7 @@
 import random
 
 
-def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets):
+def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets, clock, future_events):
     str_len = 7
 
     def seated_pass_str(passenger):
@@ -15,7 +15,8 @@ def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets):
             return "".ljust(str_len) + "|"
         else:
             return f"{passenger}:{ticket['row']},{ticket['seat']}".ljust(str_len) + "|"
-
+    print(f"Time: {clock}")
+    print(f"Future events: {future_events}")
     # Print the row numbers
     print("_" * (num_rows * (str_len + 1) + 19))
     print("Row number: ".ljust(18), end="")
@@ -44,6 +45,10 @@ def draw_plane(num_rows, num_cols, aisle_positions, seated_passengers, tickets):
 
 
 def run_simulation(num_rows, num_cols, queue, draw=False):
+    #TODO: Record simulation stats such as
+    # - Average time spent standing on the plane
+    # - Average number standing on plane at any given time
+    # - Total time spent loading the plane
     num_passengers = len(queue)
     max_seat_num = num_rows * num_cols * 2
     assert max_seat_num >= max(queue), \
@@ -80,6 +85,7 @@ def run_simulation(num_rows, num_cols, queue, draw=False):
                     service_time = random.expovariate(loading_mu)
                     future_events.append((clock + service_time, 'baggage', cur_row))
                     aisle_rows[cur_row] = next_passenger
+                    break
                 # Check if next row is available
                 elif aisle_rows[cur_row + 1] == -1:
                     # Move to next row
@@ -89,7 +95,7 @@ def run_simulation(num_rows, num_cols, queue, draw=False):
                     aisle_rows[cur_row] = next_passenger
                     break
         if draw:
-            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets)
+            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets, clock, future_events)
         # Plane loading is now in gridlock
         # Sort future events and get next event
         future_events.sort()
@@ -107,15 +113,17 @@ def run_simulation(num_rows, num_cols, queue, draw=False):
             aisle_rows[event_row] = -1
             num_seated += 1
         if draw:
-            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets)
+            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets, clock, future_events)
         # Now update the aisle positions
         for aisle_row in range(len(aisle_rows) - 1, -1, -1):
             # Skip empty aisle rows
-            if aisle_rows[cur_row] == -1:
+            if aisle_rows[aisle_row] == -1:
                 continue
-            passenger = aisle_rows[cur_row]
-            ticket = tickets[passenger]
+            next_passenger = aisle_rows[aisle_row]
+            ticket = tickets[next_passenger]
             cur_row = aisle_row
+            if cur_row == ticket['row']:
+                continue
             while cur_row <= ticket['row']:
                 if cur_row == ticket['row']:
                     # Passenger is in the row of their assigned seat
@@ -123,16 +131,19 @@ def run_simulation(num_rows, num_cols, queue, draw=False):
                     service_time = random.expovariate(loading_mu)
                     future_events.append((clock + service_time, 'baggage', cur_row))
                     aisle_rows[cur_row] = next_passenger
+                    break
                 # Check if next row is available
                 elif aisle_rows[cur_row + 1] == -1:
                     # Move to next row
+                    aisle_rows[cur_row] = -1
                     cur_row += 1
+                    aisle_rows[cur_row] = next_passenger
                 else:
                     # Next row is occupied, so stop at this row
                     aisle_rows[cur_row] = next_passenger
                     break
         if draw:
-            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets)
+            draw_plane(num_rows, num_cols, aisle_rows, occupied_seats, tickets, clock, future_events)
     return clock
 
 plane_rows = 10
@@ -140,23 +151,31 @@ plane_cols = 1
 
 times = []
 best_time = 0
+best_seed = 0
 best_ordering = []
 worst_time = 0
+worst_seed = 0
 worst_ordering = []
-for i in range(1):
+for i in range(10000):
+    random.seed(i)
     ordering = [i for i in range(plane_rows * plane_cols * 2)]
     random.shuffle(ordering)
+    random.seed(10)
     time = run_simulation(plane_rows, plane_cols, ordering.copy(), draw=False)
     times.append(time)
     if time > worst_time:
         worst_time = time
+        worst_seed = i
         worst_ordering = ordering
     elif time < best_time or best_time == 0:
         best_time = time
+        best_seed = i
         best_ordering = ordering
 
 print(f"Average time steps: {sum(times) / len(times)}")
 print(f"Best time steps: {best_time}")
 print(f"Best ordering: {best_ordering}")
+print(f"Best seed: {best_seed}")
 print(f"Worst time steps: {worst_time}")
 print(f"Worst ordering: {worst_ordering}")
+print(f"Worst seed: {worst_seed}")
